5228159a8b05078e4f3c5ddcbb716e3083b29770
paubcdev <blanescolomina.pau@gmail.com>
2023-12-02 17:53:59 +0100

    updated main.py

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ from days import *
 
 
 def aoc(day):
-    if day == 1:
-        print("Part 1: " + day01.res1 + "\n" + "Part 2: " + day01.res2)
+    if day == '1':
+        print("Part 1: " + day01.res1 + "\n" + "Part 2: " + day01.res2 + "\n")
+    elif day == '2':
+        print("Part 1: " + day02.res1 + "\n" + "Part 2: " + day02.res2 + "\n")
     else:
-        print("Specify a day between 1 and 25.")
+        print("Specify a day between 1 and 25, q to exit.")
 
 
 if __name__ == "__main__":
-    select = int(input("What day is it? "))
-    aoc(select)
+    select = ""
+    while select != 'q':
+        select = str(input("What day do you want to check? "))
+        aoc(select)
